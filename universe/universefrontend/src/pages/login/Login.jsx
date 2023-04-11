93de127be8975f0a83398390d6b4c4f07df65f2c
RuchiGupta20 <ruchigupta200901@gmail.com>
2023-04-11 10:03:30 -0400

    Added Login and Register pages to frontend

---
+++ universe/universefrontend/src/pages/login/Login.jsx
@@ -0,0 +1,23 @@
+import "./login.css"
+
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">UniVerse</h3>
+                    <span className="loginDesc"> Connect with friends, students, and clubs to share activities, events, interests, and experiences on UniVerse</span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        <input placeholder="Email" className="loginInput" />
+                        <input placeholder="Password" className="loginInput" />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">Create a New Account</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
